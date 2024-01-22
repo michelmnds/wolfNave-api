be8eaf7db7f9bd31f6fe244679e3b0dd3be4d275
michelmnds <m1tchofc@gmail.com>
2024-01-22 23:07:09 +0000

    fix: vercel.json

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("./config")(app);
 const personalTrainersRoutes = require("./routes/personalTrainers.routes");
 app.use("/personal-trainers", personalTrainersRoutes);
 
+//
 require("./error-handling")(app);
 
 module.exports = app;
