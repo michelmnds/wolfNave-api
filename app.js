1ed6ae37c59aa3121ede907d3273709d86df11e0
michelmnds <m1tchofc@gmail.com>
2024-03-04 16:28:30 +0000

    feat: added cors config

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 require("dotenv").config();
 
-const cors = require("cors");
 const express = require("express");
 const app = express();
 
 require("./config")(app);
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://wolf-nave.vercel.app",
-      "https://www.wolfnave.com/",
-    ],
-  })
-);
 
 const personalTrainersRoutes = require("./routes/personalTrainers.routes");
 app.use("/personal-trainers", personalTrainersRoutes);
